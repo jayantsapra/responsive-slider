13ceb0bf0e6da9135d89706f4daa6001991d53c3
jayantsapra <jayantsapra02@gmail.com>
2020-11-10 08:00:27 +0530

    first commit

---
+++ jquery.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    paginatiion:false,
+    autoplay:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
